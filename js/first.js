e3ee5be93500e5c6605db0a352399deb78844b22
maksiz32 <maks-manzulin@mail.ru>
2021-04-12 22:36:51 +0300

    First commit

---
+++ js/first.js
@@ -0,0 +1,29 @@
+//#1
+function isInteger(req) {
+    return (req ^ 0) === req;
+}
+
+function c2f(temp) {
+    resp = (9/5) * temp + 32;
+    //if (!Number.isInteger(resp)) { НЕ ПОДДЕРЖИВАЕТСЯ В IE
+    if (!isInteger(resp)) {
+        resp = resp.toFixed(2);
+    }
+    return resp;
+}
+
+let tempC = parseInt(prompt('Введите градусы по Цельсию:', 36.6));
+alert(c2f(tempC));
+
+//#2
+let name, admin;
+name = 'Василий';
+alert('В переменной name: '+name);
+admin = name;
+alert('В переменной admin: '+admin);
+
+//#3
+let strTest = 1000 + "108";
+let type = typeof(strTest);
+alert('Выражение 1000 + "108" создаст в переменной следующее: '+strTest+',\n'+
+' потому что происходит неявное преобразование типов в тип - '+type);
